985dc1f41e2ff915d4491e62480e0410b5ba87ae
Aira Sakuranomiya <1596356870@qq.com>
2022-06-09 12:04:33 +0800

    right click to open menu

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// $( ".desktop" ).click(function() {
+// 	$( ".menu-container" ).removeClass( "dropdown-active" );
+// });
+// 
+// $(".desktop").on("contextmenu", function() {
+// 	$( ".menu-container" ).addClass( "dropdown-active" );
+// });
+
+$(".desktop").bind('contextmenu', function (e) {
+	var id = this.id;
+	$("#txt_id").val(id);
+
+	var top = e.pageY+5;
+	var left = e.pageX;
+
+	// Show contextmenu
+	$(".menu-container").removeClass("dropdown-active").css({
+		top: top + "px",
+		left: left + "px"
+	}).delay(11).queue(function(){
+		$(".menu-container").addClass("dropdown-active").dequeue();
+	});
+
+	// disable default context menu
+	return false;
+});
+
+//$(".desktop").bind('contextmenu click',function(){
+//	$(".menu-container").removeClass("dropdown-active");
+//	$("#txt_id").val("");
+//});
+
+$( ".desktop" ).click(function() {
+	$( ".menu-container" ).removeClass( "dropdown-active" );
+});
